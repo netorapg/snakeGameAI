bfda0682fb98c3bb04ad4c5b9b8d6848fe2d3889
Argustor <renatoplatz11@outlook.com>
2023-05-30 13:49:46 -0300

    new update

--- agent.py
+++ agent.py
@@ -14,7 +14,8 @@ class Agent:
         self.n_games = 0
         self.epsilon = 0 # randomness
         self.gamma = 0 # discount rate
-        
+        self.memory = deque(maxlen=MAX_MEMORY) # popleft()
+        #TODO: model, trainer
 
     def get_state(self, game):
         pass
@@ -25,14 +26,46 @@ class Agent:
     def train_long_memory(self):
         pass
     
-    def train_short_memory(self):
+    def train_short_memory(self, state, action, reward, next_state, done):
         pass
     
     def get_action(self, state):
         pass
     
 def train():
-    pass
+    plot_scores = []
+    plot_mean_scores = []
+    total_score = 0
+    record = 0
+    agent = Agent()
+    game = SnakeGameAI()
+    
+    while True:
+        # get old state
+        state_old = agent.get_state(game)
+        # get move
+        final_move = agent.get_action(state_old)
+        # perform move and get new state
+        reward, done, score = game.play_step(final_move)
+        # set reward for the new state
+        state_new = agent.get_state(game)
+        # train short memory
+        agent.train_short_memory(state_old, final_move, reward, state_new, done)
+        # remember
+        agent.remember(state_old, final_move, reward, state_new, done)
+        # train long memory
+        if done:
+            # train long memory, plot result
+            game.reset()
+            agent.n_games += 1
+            agent.train_long_memory()
+            
+            if score > record:
+                record = score
+                #agent.model.save()
+                
+                print('Game', agent.n_games, 'Score', score, 'Record', record)
+
 
 if __name__ == '__main__':
     train()
